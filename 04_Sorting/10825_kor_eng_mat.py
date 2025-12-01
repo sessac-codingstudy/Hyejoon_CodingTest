3a9b89d22c9be77b535800be16adbd8674976b76
mhjoon99 <hyejoon8@gmail.com>
2025-12-01 12:43:17 +0900

    10825번 주석 추가 및 lambda 활용한 sol 추가

--- 04_Sorting/10825_kor_eng_mat.py
+++ 04_Sorting/10825_kor_eng_mat.py
@@ -9,14 +9,20 @@
 """
 n = int(input())
 array = []
+
+# 공백을 기준으로 튜플 형태로 입력받기
 for i in range(n):
     input_data = input().split()
     array.append((input_data[0], int(input_data[1])))
+# print(array)
 
+# sorted() 함수의 key 매개변수 조건설정
 def setting(student):
     return student[1]
 
 array = sorted(array, key=setting)
+# lambda를 활용한 다른 방식
+# array = sorted(array, key=lambda student: student[1])
 
 for student in array:
     print(student[0], end=' ')
