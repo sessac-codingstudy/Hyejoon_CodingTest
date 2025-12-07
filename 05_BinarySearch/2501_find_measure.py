4564fa1e288262da900bf833d19c1027808c307d
mhjoon99 <hyejoon8@gmail.com>
2025-12-08 02:49:13 +0900

    백준 2501번 약수 구하기 문제

---
+++ 05_BinarySearch/2501_find_measure.py
@@ -0,0 +1,33 @@
+"""
+파일명: 2501_find_measure.py
+작성일: 2025-12-08
+문제 링크: https://www.acmicpc.net/problem/2501
+백준 난이도: Bronze 3
+체감 난이도: ⭐
+
+설명: 약수 구하기 문제 (Greedy) -> 이진 트리로 풀기
+"""
+
+
+
+# # sol1) 완전탐색(greedy) => 런타임 에러가 나옴
+# n, k = map(int, input().split())
+# list = []
+
+# for i in range(n):
+#     if n % (i+1) == 0:
+#         list.append(i+1)
+# # print(list)
+# print(list[k-1])
+
+# sol2) k번째까지만 찾기
+n, k = map(int, input().split())
+
+for i in range(1, n+1):
+    if n % (i) == 0:
+        k -= 1
+        if k == 0:
+            print(i)
+            break
+else:
+    print(0)
